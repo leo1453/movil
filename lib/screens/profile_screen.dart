7d27fe6d719fc686a521a00313a3b564571838b1
leo <ldkl12@hotmail.com>
2025-04-25 17:04:33 -0600

    perfil de usuario

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -8,22 +10,52 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   final _formKey = GlobalKey<FormState>();
 
-  // Controladores para los campos
-  final TextEditingController _nameController = TextEditingController(
-    text: 'Juan Pérez',
-  );
-  final TextEditingController _emailController = TextEditingController(
-    text: 'juan.perez@example.com',
-  );
-  final TextEditingController _addressController = TextEditingController(
-    text: 'Av. Principal 123, Ciudad de México',
-  );
-  final TextEditingController _phoneController = TextEditingController(
-    text: '555-1234-567',
-  );
+  // Controladores
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _addressController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
 
-  void _saveProfile() {
-    if (_formKey.currentState!.validate()) {
+  final User? user = FirebaseAuth.instance.currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  void _loadUserData() async {
+    if (user != null) {
+      _emailController.text = user!.email ?? '';
+
+      // Intentamos traer los datos extra de Firestore
+      final snapshot =
+          await FirebaseFirestore.instance
+              .collection('usuarios')
+              .doc(user!.uid)
+              .get();
+
+      if (snapshot.exists) {
+        final data = snapshot.data();
+        _nameController.text = data?['nombre'] ?? '';
+        _addressController.text = data?['direccion'] ?? '';
+        _phoneController.text = data?['telefono'] ?? '';
+      }
+    }
+  }
+
+  void _saveProfile() async {
+    if (_formKey.currentState!.validate() && user != null) {
+      await FirebaseFirestore.instance
+          .collection('usuarios')
+          .doc(user!.uid)
+          .set({
+            'nombre': _nameController.text,
+            'direccion': _addressController.text,
+            'telefono': _phoneController.text,
+            'email': _emailController.text,
+          }, SetOptions(merge: true)); // Actualizar sin borrar otros datos
+
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Perfil actualizado correctamente')),
       );
@@ -69,72 +101,79 @@ class _ProfileScreenState extends State<ProfileScreen> {
         backgroundColor: Colors.deepPurple,
         iconTheme: IconThemeData(color: Colors.white),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              CircleAvatar(
-                radius: 50,
-                backgroundColor: Colors.deepPurple,
-                child: Icon(Icons.person, size: 50, color: Colors.white),
-              ),
-              SizedBox(height: 16),
-              buildTextField(
-                label: 'Nombre completo',
-                controller: _nameController,
-                icon: Icons.person,
-              ),
-              buildTextField(
-                label: 'Correo electrónico',
-                controller: _emailController,
-                icon: Icons.email,
-                inputType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese su correo';
-                  }
-                  if (!value.contains('@')) {
-                    return 'Ingrese un correo válido';
-                  }
-                  return null;
-                },
-              ),
-              buildTextField(
-                label: 'Dirección',
-                controller: _addressController,
-                icon: Icons.home,
-              ),
-              buildTextField(
-                label: 'Teléfono',
-                controller: _phoneController,
-                icon: Icons.phone,
-                inputType: TextInputType.phone,
-              ),
-              SizedBox(height: 24),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.deepPurple,
-                    padding: EdgeInsets.symmetric(vertical: 16),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                  onPressed: _saveProfile,
-                  icon: Icon(Icons.save, color: Colors.white),
-                  label: Text(
-                    'Guardar cambios',
-                    style: TextStyle(color: Colors.white),
+      body:
+          user == null
+              ? Center(child: Text('Debes iniciar sesión'))
+              : SingleChildScrollView(
+                padding: const EdgeInsets.all(16.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 50,
+                        backgroundColor: Colors.deepPurple,
+                        child: Icon(
+                          Icons.person,
+                          size: 50,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      buildTextField(
+                        label: 'Nombre completo',
+                        controller: _nameController,
+                        icon: Icons.person,
+                      ),
+                      buildTextField(
+                        label: 'Correo electrónico',
+                        controller: _emailController,
+                        icon: Icons.email,
+                        inputType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor ingrese su correo';
+                          }
+                          if (!value.contains('@')) {
+                            return 'Ingrese un correo válido';
+                          }
+                          return null;
+                        },
+                      ),
+                      buildTextField(
+                        label: 'Dirección',
+                        controller: _addressController,
+                        icon: Icons.home,
+                      ),
+                      buildTextField(
+                        label: 'Teléfono',
+                        controller: _phoneController,
+                        icon: Icons.phone,
+                        inputType: TextInputType.phone,
+                      ),
+                      SizedBox(height: 24),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.deepPurple,
+                            padding: EdgeInsets.symmetric(vertical: 16),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                          onPressed: _saveProfile,
+                          icon: Icon(Icons.save, color: Colors.white),
+                          label: Text(
+                            'Guardar cambios',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
